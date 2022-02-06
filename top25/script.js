7c7dcf96b455c3a3c9adc251ac0aad7838b09f63
davraya <davraya4@gmail.com>
2022-02-05 19:18:29 -0700

    finished top 25

--- top25/script.js
+++ top25/script.js
@@ -2,22 +2,26 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector(".list");
 
+
 function add_to_list (){
+    let div = document.createElement("div")
+    list.appendChild(div)
     let new_li = document.createElement("li");
     new_li.textContent = input.value;
-    list.appendChild(new_li);
-    new_li.innerHTML += `<button class="${input.value}">❌</button>`
-    const books = Array.from(list.children)
-    books.forEach(book => console.log(this))
-    // console.log(books)
-    // console.log(Array.from(list.children))
+    div.appendChild(new_li)
+    // new_li.innerHTML += `<button class="button_${input.value}">❌</button>`
+    // new_button = document.querySelector(`.button_${input.value}`) 
+    // new_button.addEventListener("click", del_book(input.value))
+    let new_button = document.createElement("button")
+    new_button.textContent = "❌"
+    div.appendChild(new_button)
+    new_button.addEventListener("click", () => {
+        list.removeChild(div)
+    })
+
 }
 
-function del_item (button) {
-    // document.querySelector(".")
-    console.log(button)
 
-}
 
 button.addEventListener("click", add_to_list);
 

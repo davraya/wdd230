e38d2d5ebd45884b73b898bf17e7ad7a924042b1
David Raya <davraya4@gmail.com>
2022-04-04 17:19:31 -0600

    test in new laptop

---
+++ final-website/js/ham.js
@@ -0,0 +1,3 @@
+const ham = document.querySelector(".ham");
+const navigation = document.querySelector(".navigation");
+ham.addEventListener("click", () => {navigation.classList.toggle("responsive")});
